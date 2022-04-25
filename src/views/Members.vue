80ed69aa19db81034fb1c65e7b22e8a0dd16d7c5
sor4chi <sorakawa0218@gmail.com>
2022-04-25 12:16:32 +0900

    Init

---
+++ src/views/Members.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>a</div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+
+export default {
+  name: "Members",
+  props: {
+    user: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    const members = ref([]);
+
+    return {
+      members,
+    };
+  },
+};
+</script>
