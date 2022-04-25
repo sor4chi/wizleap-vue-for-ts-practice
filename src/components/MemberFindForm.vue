54758233ebcd164656b237d74ae9422da88eee1e
sor4chi <sorakawa0218@gmail.com>
2022-04-25 17:43:16 +0900

    Create find system

---
+++ src/components/MemberFindForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <input type="text" v-model="findTextLocal" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watch } from "vue";
+
+export default defineComponent({
+  name: "MemberFindForm",
+  props: {
+    findText: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props, { emit }) {
+    const findTextLocal = ref(props.findText);
+
+    watch(findTextLocal, () => {
+      emit("updateFindText", findTextLocal.value);
+    });
+
+    return {
+      findTextLocal,
+    };
+  },
+});
+</script>
--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <MemberFindForm :findText="findText" @updateFindText="updateFindText" />
     <div v-for="member in filteredMembers" :key="member.id">
       <hr />
       <MemberCard :member="member" />
@@ -8,16 +9,18 @@
 </template>
 
 <script lang="ts">
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, defineComponent } from "vue";
 import axios from "axios";
 
 import { Member } from "types/Member";
 import MemberCard from "@/components/MemberCard.vue";
+import MemberFindForm from "@/components/MemberFindForm.vue";
 
-export default {
+export default defineComponent({
   name: "Members",
   components: {
     MemberCard,
+    MemberFindForm,
   },
   setup() {
     const members = ref<Member[]>([]);
@@ -39,13 +42,19 @@ export default {
       members.value = data;
     };
 
+    const updateFindText = (newFindText: string) => {
+      findText.value = newFindText;
+    };
+
     onMounted(() => {
       fetchMembers();
     });
 
     return {
       filteredMembers,
+      findText,
+      updateFindText,
     };
   },
-};
+});
 </script>
